68395ce5bca38e869b9138d5ce6de14312faa270
danuts <matei1337@icloud.com>
2022-04-04 13:03:47 +0300

    All jQuery has been converted to JavaScript.

--- rating-component/assets/js/rating.js
+++ rating-component/assets/js/rating.js
@@ -1,22 +1,39 @@
-$('.rating-level').on('click', (function() {
-    $(this).addClass('selected');
-    $(this).prevAll().addClass('selected');
-    $(this).nextAll().removeClass('selected');
-    var count = $(this).data('rating');
-    for(var i = 0; i < count - 1; i ++) {
-        $('.rating-level').eq(i).removeClass('selected');
-    }
-}));
+const rating_level = document.querySelectorAll('.rating-level');
 
-if($('.submit').click(function() {
-    var check_rating = $('.rating-levels').find('.selected').length;
-    if(!check_rating) {
-        $('.alert-text').text('Please rate us!');
+rating_level.forEach(function(item, index) {
+    (function(i) {
+        rating_level[i].addEventListener("click", function() {
+            if(this.classList.contains("selected")) {
+                this.classList.remove("selected");
+            } else {
+                this.classList.add("selected");
+            }
+        
+            var count = this.getAttribute('data-rating');
+        
+            for(var i = 0; i < count - 1; i ++) {
+                document.querySelectorAll('.rating-level')[i].classList.remove('selected');
+            }
+        });
+    }(index))
+});
+
+const submit_rating = document.getElementById('submit');
+
+submit_rating.onclick = function sendRating() {
+    const rating_levels = document.getElementById('rating-levels');
+    const check_rating = rating_levels.querySelectorAll('.selected');
+
+    if(!check_rating.length) {
+        var alert_text = document.querySelector('.alert-text');
+        alert_text.textContent = "Please rate us.";
     } else {
         document.getElementById("thank-you").removeAttribute("hidden");
         document.getElementById("rating").setAttribute("hidden", "hidden");
 
-        var selectedRating = $('.rating-level.selected').data('rating');
-        $('.selected-rating-text').text('You selected ' + selectedRating + ' out of 5.');
+        var selectedRating = document.querySelector('.rating-level.selected').getAttribute('data-rating');
+        var selectedRatingText = document.querySelector('.selected-rating-text');
+
+        selectedRatingText.textContent = 'You selected ' + selectedRating + ' out of 5.';
     }
-}));
+}
