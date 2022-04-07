8b596c6607f1cb8cd5c6e50c093a9ef2f0db10ab
danuts <matei1337@icloud.com>
2022-04-07 20:33:56 +0300

    Solved a bug where when selecting numbers from 5 to 1, the `selected` class doesn't got removed from the numbers before.

--- rating-component/assets/js/rating.js
+++ rating-component/assets/js/rating.js
@@ -1,24 +1,33 @@
-var ratingLevel = document.querySelectorAll('.rating-level');
+const ratingLevel = document.querySelectorAll('.rating-level');
 
-ratingLevel.forEach(function(rating, index) {
-    (function(i) {
-        ratingLevel[i].addEventListener("click", function() {
+ratingLevel.forEach((rating, index) => {
+    ((rating) => {
+        ratingLevel[index].addEventListener("click", function() {
             this.classList.contains("selected") ? this.classList.remove("selected") : this.classList.add("selected");
-        
+
             var dataRating = this.getAttribute('data-rating');
-        
-            for(var i = 0; i < dataRating - 1; i ++) {
-                document.querySelectorAll('.rating-level')[i].classList.remove('selected');
+
+            for(var i = 0; i < ratingLevel.length; i ++) {
+                console.log(dataRating);
+                // console.log(i)
+
+                if(ratingLevel[i].getAttribute('data-rating') != dataRating) {
+                    ratingLevel[i].classList.remove("selected");
+                }
             }
+
+            // for(var i = 0; i < dataRating - 1; i ++) {
+            //     document.querySelectorAll('.rating-level')[i].classList.remove('selected');
+            // }
         });
-    }(index))
+    })(rating);
 });
 
 var submitRating = document.getElementById('submit');
 
 submitRating.onclick = function sendRating() {
     var ratingLevels = document.getElementById('rating-levels');
-    var checkRating = ratingLevels.querySelectorAll('.selected');
+    const checkRating = ratingLevels.querySelectorAll('.selected');
 
     if(!checkRating.length) {
         var alertText = document.querySelector('.alert-text');
